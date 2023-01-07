0e8963ffb8078c656f9237efb57b5d39aff6c5e0
Sonny-in-1999 <121083122+Sonny-in-1999@users.noreply.github.com>
2023-01-07 13:15:22 +0900

    router

---
+++ README.md
@@ -0,0 +1,17 @@
+Router
+
+#Global Router
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search a video
+
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+/videos/watch -> Watch video
+/videos/edit -> Edit video
+/videos/delete -> Delete video
+/videos/comments -> Comment on a video
+/videos/comments/delete -> Delete comment of a video
--- src/server.js
+++ src/server.js
@@ -1,41 +1,36 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev"); //return middleware
+app.use(logger);
 
-const logger = (req, res, next) => { //middleware 
-    console.log(`${req.method} ${req.url}`);
-    next(); //다음 펑션을 수행
-};
+const globalRouter = express.Router();
 
-const privateMiddleware = (req, res, next) => { //middleware
-    const url = req.url;
-    if(url == "/protected"){
-        return res.send("<h1>You Are Not Allowed!</h1>");
-    } //This middleware is controller.
-    console.log("Allowed. You may continue.");
-    next(); //다음 펑션을 수행
-};
+const handleHome = (req, res) => res.send("Home");
 
-const handleHome = (req, res) => { //finalware
-    return res.send("<h1>Middleware alive!</h1>");
-};
+globalRouter.get("/", handleHome);
 
-const handleLogin = (req, res) => {
-    return res.send({message: "Login here."});
-};
+const userRouter = express.Router();
 
-const handleProtected = (req, res) => {
-    return res.send("Welcome to the private lounge.");
-};
+const handleEditUser = (req, res) => res.send("Edit User");
 
-app.use(logger); //middleware 사용, 반드시 get보다 위에 있어야함
-app.use(privateMiddleware); //use를 사용함으로써 모든 url에 middleware를 적용함.
-app.get("/", handleHome);   // "/" = router(path) 문 같은 역할
-app.get("/login", handleLogin); 
-app.get("/protected", handleProtected);
-//app.get(path, middleware, finalware)의 형식으로 하나의 url에만 middleware를 적용할 수 있음
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
+
+
+
 const handleListening = () => console.log(`Server listening on http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening); //server 시작시 출력
