0c3ea1fb64fecc4edeb771bc49ff7c949d7c34bb
Sonny-in-1999 <121083122+Sonny-in-1999@users.noreply.github.com>
2023-01-07 11:19:05 +0900

    test

--- src/server.js
+++ src/server.js
@@ -4,4 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server listening on port http://localhost:${PORT}`));
+const handleHome = (req, res) => {
+    return res.send("<h1>You can't access this page</h1>");
+};
+const handleLogin = (req,res) => {
+    return res.send({message: "Login here."});
+}
+
+app.get("/", handleHome);   //request (브라우저가 홈페이지 요청)
+app.get("/login", handleLogin);
+
+const handleListening = () => console.log(`Server listening on http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening); //server 시작시 출력
