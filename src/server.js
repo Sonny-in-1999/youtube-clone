ff587b49faefe5fec5d16ed2f0df5c7a4ea9a4b7
Sonny-in-1999 <121083122+Sonny-in-1999@users.noreply.github.com>
2023-01-07 11:49:15 +0900

    Using middleware 2
    use = 모든 url에 펑션(미들웨어) 적용
    하나의 url에만 적용하고 싶을 경우 get에 기입.
    use가 get보다 먼저 입력되어야 미들웨어가 수행된다.

--- src/server.js
+++ src/server.js
@@ -4,21 +4,38 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => { //middleware
-    console.log(`Someone is going to: ${req.url}`);
-    next();
-}
+const logger = (req, res, next) => { //middleware 
+    console.log(`${req.method} ${req.url}`);
+    next(); //다음 펑션을 수행
+};
+
+const privateMiddleware = (req, res, next) => { //middleware
+    const url = req.url;
+    if(url == "/protected"){
+        return res.send("<h1>You Are Not Allowed!</h1>");
+    } //This middleware is controller.
+    console.log("Allowed. You may continue.");
+    next(); //다음 펑션을 수행
+};
 
 const handleHome = (req, res) => { //finalware
     return res.send("<h1>Middleware alive!</h1>");
 };
+
 const handleLogin = (req, res) => {
     return res.send({message: "Login here."});
-}
+};
 
-app.get("/", gossipMiddleware, handleHome);   //request (브라우저가 홈페이지 요청)
-app.get("/login", handleLogin);
+const handleProtected = (req, res) => {
+    return res.send("Welcome to the private lounge.");
+};
 
+app.use(logger); //middleware 사용, 반드시 get보다 위에 있어야함
+app.use(privateMiddleware); //use를 사용함으로써 모든 url에 middleware를 적용함.
+app.get("/", handleHome);   //request (브라우저가 홈페이지 요청)
+app.get("/login", handleLogin); 
+app.get("/protected", handleProtected);
+//app.get(path, middleware, finalware)의 형식으로 하나의 url에만 middleware를 적용할 수 있음
 const handleListening = () => console.log(`Server listening on http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening); //server 시작시 출력
