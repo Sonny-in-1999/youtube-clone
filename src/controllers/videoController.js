403c0191acc1f81bd99169f4c6a1d3d81aa3dd07
Sonny-in-1999 <121083122+Sonny-in-1999@users.noreply.github.com>
2023-01-11 21:52:49 +0900

    update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -7,14 +7,18 @@ videoModel.find({}, (error, videos) => {
 //{}: Search Term(비어있을 경우, 모든 형식을 search), db가 반응할경우 mongoose가 function 실행!
 
 export const home = async(req, res) => { //promise 형식으로 작성되었음 (Not callback)
-    try { //에러가 발생하지 않을경우 그대로 실행
+    try { //실행
         const videos = await videoModel.find({});
+        //videoModel.find({}) => db에 있는 video를 불러옴
         //await: callback이 필요하지 않음을 의미(표시)! await이 db를 기다려줌 (중요)await은 async function 안에서만 사용가능!!
-        res.render("home", {pageTitle: "Home", videos: []});
+        console.log(videos);
+        res.render("home", {pageTitle: "Home", videos});
         //template를 rendering
         //return이 아닌 function(render) 그 자체에 집중할 것!
-    } catch{ //에러가 발생할 경우 실행
-        return res.render("server-error");
+    } catch(error){ //try에서 에러가 발생할 경우 catch로 점프!
+        console.log(error);
+        return res.render("upload", { pageTitle: "Upload Video", errorMessage: error._message })
+        //error가 발생할 경우 upload페이지를 다시 렌더링!
     }
 };
 
@@ -39,9 +43,20 @@ export const getUpload = (req, res) => {
     return res.render("upload", {pageTitle: "Upload Video"});
 };
 
-export const postUpload = (req, res) => {
-    const { title } = req.body;
+export const postUpload = async (req, res) => {
+    const { title, description, hashtags } = req.body;
     //name="title"인 input(upload.pug의 text)에서 req.body(input의 내용)을 받아옴!
+    await videoModel.create({
+        title,
+        description,
+        hashtags: hashtags.split(",").map((word) => `#${word}`),
+        meta: {
+            views: {type:Number, default: 0, required: true},
+            rating: {type:Number, default: 0, required: true},
+        },
+    });
+    //await video.save();
+    //video.save() => 생성된 video를 return 해줌 (db에 저장!)
     return res.redirect("/");
 };
 
--- src/models/video.js
+++ src/models/video.js
@@ -2,19 +2,20 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    createdAt: Date,
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    createdAt: { type:Date, required: true, default: Date.now },
+    //Date.now() X Date.now O => 내가 새로운 video를 생성했을 때만 실행시키기위해
+    //()를 붙일경우 function을 즉각 실행시킴
     hashtags: [{type:String}],
     meta: {
         views: Number,
         rating: Number,
     },
 });
-//video의 형식(schema)지정
+//video(model)의 형식(schema)지정
 
 const videoModel = mongoose.model("Video", videoSchema);
 //video model 지정
-
 
 export default videoModel;
