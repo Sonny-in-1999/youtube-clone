79f161fabe0862baaac59c47dd23a910b6201f32
Sonny-in-1999 <121083122+Sonny-in-1999@users.noreply.github.com>
2023-01-12 17:10:04 +0900

    cookie(session id)
    each browser send different session id

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -20,6 +21,27 @@ app.use(logger);
 app.use(express.urlencoded({extended:true}));
 //form의 value를 리턴해주는 middleware ==> form을 javascript로 변환해줌
 //이 구간에서 req.body 생성
+
+
+app.use(
+    session({
+      secret: "Hello!"
+    })
+  );
+
+app.use((req, res, next) => {
+    req.sessionStore.all((error, sessions) => {
+        console.log(sessions);
+        //browser마다 다른 cookie(session id)를 보냄. browser 구분 가능.
+        next();
+    });
+});
+
+app.get("/add-one", (req, res, next) => {
+    req.session.potato += 1;
+    return res.send(`${req.session.id}\n${req.session.potato}`);
+})
+
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
